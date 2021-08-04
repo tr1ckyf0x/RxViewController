392a39b7907f10426747a91ca79142b3d8559f13
Vladislav Lisianskii <vladislav.lisyanskiy@gmail.com>
2021-08-04 04:42:16 +0300

    Bumped iOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "RxViewController",
   platforms: [
-    .macOS(.v10_11), .iOS(.v8), .tvOS(.v9)
+    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
   ],
   products: [
     .library(name: "RxViewController", targets: ["RxViewController"]),
